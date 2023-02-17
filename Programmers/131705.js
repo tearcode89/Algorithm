e3f37137c091787c1ed4cb3fc3c4013284f67efb
sangjun <sjun8932.3@gmail.com>
2023-02-17 21:34:23 +0900

    '프로그래머스 삼총사'

---
+++ Programmers/131705.js
@@ -0,0 +1,17 @@
+// lv 1. 삼총사
+// https://school.programmers.co.kr/learn/courses/30/lessons/131705
+function solution(number) {
+    var answer = 0;
+    for(var i=0; i<number.length; i++){
+        for(var j=i+1; j<number.length; j++){
+            for(var k=j+1; k<number.length; k++){
+                if(number[i] + number[j] + number[k] === 0){
+                    answer += 1;
+                }
+            }
+        }
+    }
+    return answer;
+}
+
+console.log(solution([-2,3,0,2,-5]))
