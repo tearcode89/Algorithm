90d94241447631829a2afde3dd42db596abd879b
sjun8932.3 <sjun8932.3@gmail.com>
2023-03-15 23:12:36 +0900

    '프로그래머스 lv. 1 내적'

---
+++ Programmers/70128.js
@@ -0,0 +1,10 @@
+// lv. 1 내적
+// https://school.programmers.co.kr/learn/courses/30/lessons/70128
+
+function solution(a, b) {
+    var answer = 0;
+    for (let i = 0; i < a.length; i++) {
+        answer += a[i]*b[i];
+    }
+    return answer;
+}
